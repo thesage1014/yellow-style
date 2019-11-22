80f2619046c1a79521ee2d4fe803d1b52fc9b57b
Steve Fisher <36834391+fischsauce@users.noreply.github.com>
2019-11-22 12:27:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,43 +11,13 @@ apt-install() {
 #install ffmpeg to container
 add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
 apt-install || exit 1
-#create folders
-
-# mkdir models
-
-
-# cd /storage
-# mkdir data
-# cd data
-
-# mkdir vgg
-# mkdir train
-
-# cd train
-# wget http://173.255.194.181:5000/simpsons.tar.gz
-# tar -xzf simpsons.tar.gz
-# rm simpsons.tar.gz
-
-# cd vgg
-# wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-
-# cd /paperspace
-
-
-#run style transfer on video
-# python transform_video.py --in-path examples/content/fox.mp4 \
-#   --checkpoint ./scream.ckpt \
-#   --out-path /artifacts/out.mp4 \
-#   --device /gpu:0 \
-#   --batch-size 4 2>&1
-# wget https://www.broadcastingcable.com/.image/t_share/MTU3NTE4OTY0OTYyODk1MDUw/patrick-stewart-picard-star-trek-cbs.jpg
 
 pip install moviepy
 
-python3 style.py --checkpoint-dir /artifacts \
+python style.py --checkpoint-dir /artifacts \
 		 --train-path /datasets/coco \
 		 --epochs 2 \
 		 --batch-size 4 \
-		 --checkpoint-iterations 1000 \
+		 --checkpoint-iterations 2000 \
 		 --vgg-path /storage/vgg/imagenet-vgg-verydeep-19.mat \
 		 --style /storage/burns.jpg 2>&1
