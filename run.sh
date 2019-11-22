19678d2aa862cfdc52a852d34bc9e47161121fc4
Steve Fisher <36834391+fischsauce@users.noreply.github.com>
2019-11-22 13:02:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
-apt-install() {
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get update -q
-	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
-	return $?
-}
+# APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
+# apt-install() {
+# 	export DEBIAN_FRONTEND=noninteractive
+# 	apt-get update -q
+# 	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
+# 	return $?
+# }
 
-#install ffmpeg to container
-add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
-apt-install || exit 1
+# #install ffmpeg to container
+# add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
+# apt-install || exit 1
+
+apt-get update
+apt-get install wget -y
+apt-get update && apt-get install -y software-properties-common
+
+# Install "ffmpeg"
+add-apt-repository ppa:mc3man/xerus-media
+apt-get update && apt-get install -y ffmpeg
+
+mkdir /artifacts/checkpoints
 
 pip install moviepy Pillow scipy numpy
 
